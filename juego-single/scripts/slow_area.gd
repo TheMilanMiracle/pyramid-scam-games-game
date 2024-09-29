09ed10af210db952d3b1e42cfe3a49c1a432709e
Luciano Márquez Castro <lxziano.mc@gmail.com>
2024-09-29 18:22:17 -0300

    added UI elements for cooldown and scenery for ver0.1.0

--- juego-single/scripts/slow_area.gd
+++ juego-single/scripts/slow_area.gd
@@ -2,7 +2,12 @@ extends Area2D
 
 @onready var slow_area_sprite: Sprite2D = $SlowAreaSprite
 @onready var slow_area_shape: CollisionShape2D = $SlowAreaShape
-@onready var action_timer: Timer = $"ActionTimer"
+
+@onready var cooldown_timer: Timer = %CooldownTimer
+@onready var action_timer: Timer = %ActionTimer
+@onready var cooldown_progress: TextureProgressBar = %CooldownProgress
+@onready var area_progress: TextureProgressBar = %AreaProgress
+
 @onready var blue_particles: CPUParticles2D = $BlueParticles
 @onready var cyan_particles: CPUParticles2D = $CyanParticles
 
@@ -19,20 +24,28 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	pass
+	if cooldown_timer.time_left > 0:
+		cooldown_progress.value =  (cooldown_timer.wait_time - cooldown_timer.time_left) / cooldown_timer.wait_time * 100
+	
+	if action_timer.time_left > 0:
+		area_progress.value = (action_timer.wait_time - action_timer.time_left) / action_timer.wait_time * 100
 
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
 		match event.button_index:
 			MOUSE_BUTTON_RIGHT:
-				position = get_viewport().get_camera_2d().get_global_mouse_position()
-				slow_area_shape.disabled = false
-				slow_area_sprite.visible = true
-				action_timer.start()
-				
-				blue_particles.emitting = true
-				cyan_particles.emitting = true
+				if cooldown_timer.time_left == 0.:
+					position = get_viewport().get_camera_2d().get_global_mouse_position()
+					slow_area_shape.disabled = false
+					slow_area_sprite.visible = true
+					action_timer.start()
+					
+					blue_particles.emitting = true
+					cyan_particles.emitting = true
+					
+					cooldown_timer.start()
+					cooldown_progress.value = 0
 
 
 func _on_action_timer_timeout() -> void:
