d3358ac4355547bfc85a3c19af7d12a0fe66c443
Luciano Márquez Castro <lxziano.mc@gmail.com>
2024-09-28 23:06:33 -0300

    update slow area behaviour to slow bullets movement

--- juego-single/scripts/enemy_bullet.gd
+++ juego-single/scripts/enemy_bullet.gd
@@ -1,8 +1,13 @@
+class_name EnemyBullet
 extends Area2D
 
-var speed = 300
+signal bullet_died()
 
-# Called when the node enters the scene tree for the first time.
+const speed:int = 300
+
+var default_delta_multiplier: float = 1.
+var delta_multiplier: float = 1.
+
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	area_entered.connect(_on_area_entered)
@@ -10,14 +15,20 @@ func _ready() -> void:
 func take_damage_en():
 	queue_free()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _physics_process(delta: float) -> void:
+	delta *= delta_multiplier
+	
 	position += speed * transform.y * delta
+
 
 func _on_body_entered(body: Node2D):
 	if body.has_method("take_damage"):
 		body.take_damage()
 		queue_free()
+	
+	bullet_died.emit()
+
 
 func _on_area_entered(body: Area2D):
 	if body.has_method("take_damage"):
@@ -25,3 +36,13 @@ func _on_area_entered(body: Area2D):
 		queue_free()
 	if body.has_method("defense"):
 		queue_free()
+	
+	bullet_died.emit()
+
+
+func slow_down(multiplier: float) -> void:
+	delta_multiplier = multiplier
+
+
+func speed_up() -> void:
+	delta_multiplier = default_delta_multiplier
--- juego-single/scripts/slow_area.gd
+++ juego-single/scripts/slow_area.gd
@@ -3,8 +3,15 @@ extends Area2D
 @onready var slow_area_sprite: Sprite2D = $SlowAreaSprite
 @onready var slow_area_shape: CollisionShape2D = $SlowAreaShape
 @onready var action_timer: Timer = $"ActionTimer"
+@onready var blue_particles: CPUParticles2D = $BlueParticles
+@onready var cyan_particles: CPUParticles2D = $CyanParticles
+
+@export var SLOW: float = 0.2
 
 func _ready() -> void:
+	area_entered.connect(_on_area_entered)
+	area_exited.connect(_on_area_exited)
+	
 	action_timer.timeout.connect(_on_action_timer_timeout)
 	
 	slow_area_shape.disabled = true
@@ -18,13 +25,29 @@ func _process(delta: float) -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
 		match event.button_index:
-			MOUSE_BUTTON_LEFT:
+			MOUSE_BUTTON_RIGHT:
 				position = get_viewport().get_camera_2d().get_global_mouse_position()
 				slow_area_shape.disabled = false
 				slow_area_sprite.visible = true
 				action_timer.start()
+				
+				blue_particles.emitting = true
+				cyan_particles.emitting = true
 
 
 func _on_action_timer_timeout() -> void:
 	slow_area_shape.disabled = true
 	slow_area_sprite.visible = false
+	
+	blue_particles.emitting = false
+	cyan_particles.emitting = false
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.has_method("slow_down") and area.has_method("speed_up"):
+		area.slow_down(SLOW)
+
+
+func _on_area_exited(area: Area2D) -> void:
+	if area.has_method("slow_down") and area.has_method("speed_up"):
+		area.speed_up()
