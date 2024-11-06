bcb0d74dcac51a187849ba08ea009e158efb1a4a
Dagt07 <davidgarciat07@gmail.com>
2024-11-06 09:45:56 -0300

    ADD: victory and death menu -> main menu button link to .tscn

--- juego-single/scripts/UI/death_menu.gd
+++ juego-single/scripts/UI/death_menu.gd
@@ -16,7 +16,8 @@ func _on_try_again_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
 
 func _on_main_menu_pressed() -> void:
-	pass
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/UI/main_menu.tscn")
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
