715ff1cafc6387a8e460aa901542a5c6c2798d4f
Dagt07 <davidgarciat07@gmail.com>
2024-12-10 20:13:34 -0300

    ADD: more details

---
+++ juego-single/scripts/rooms/room_03.gd
@@ -0,0 +1,11 @@
+extends room
+
+@onready var tile_map_layer_2: TileMapLayer = $TileMapLayer2
+@onready var tile_map_layer_3: TileMapLayer = $TileMapLayer3
+
+func _ready() -> void:
+	enemy_count = 7
+
+func updateRoomState() -> void:
+	if enemy_count == 4:
+		tile_map_layer_2.queue_free()
